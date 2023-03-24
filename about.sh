9957973ea8505b3bac192923188a5db5c6993897
qorsan73 <99475446+qorsan73@users.noreply.github.com>
2023-03-25 00:42:24 +0300

    Create about.sh

---
+++ about.sh
@@ -0,0 +1,45 @@
+echo -e "\033[1;96mPress ENTER to continue"
+read arya
+echo -e "\033[1;90mLoading....."
+echo
+sleep 0.6
+clear
+echo
+
+echo -e "\033[1;91m ▄▄▄       ▄▄▄▄    ▒█████   █    ██ ▄▄▄█████▓
+▒████▄    ▓█████▄ ▒██▒  ██▒ ██  ▓██▒▓  ██▒ ▓▒
+▒██  ▀█▄  ▒██▒ ▄██▒██░  ██▒▓██  ▒██░▒ ▓██░ ▒░
+░██▄▄▄▄██ ▒██░█▀  ▒██   ██░▓▓█  ░██░░ ▓██▓ ░ 
+ ▓█   ▓██▒░▓█  ▀█▓░ ████▓▒░▒▒█████▓   ▒██▒ ░ 
+ ▒▒   ▓▒█░░▒▓███▀▒░ ▒░▒░▒░ ░▒▓▒ ▒ ▒   ▒ ░░   
+  ▒   ▒▒ ░▒░▒   ░   ░ ▒ ▒░ ░░▒░ ░ ░     ░    
+  ░   ▒    ░    ░ ░ ░ ░ ▒   ░░░ ░ ░   ░      
+      ░  ░ ░          ░ ░     ░              
+                ░                            
+"
+
+echo -e "\033[1;92m[\033[1;97m#\033[1;92m]\033[1;93m losiphar: \033[1;92mlosiphar is a shell programmed tool for Termux and Linux, That can capture voice of your victim. When your victim will open your link then you will receiving audio file in wav frequency from your victim.
+\033[1;92m[\033[1;97m#\033[1;92m] \033[1;93mWorking: \033[1;92mAfter the user grants microphone permissions, a website redirect button of your choice is released to distract the target while small audio files (about 4 seconds in wav format) are sent to the attacker. It uses [Recorderjs], plugin for recording/exporting the output of Web Audio API nodes.
+\033[1;92m[\033[1;97m#\033[1;92m]\033[1;93m Port forwarding: \033[1;92mHere you can create phishing link using two different types port forwarding.
+     \033[1;92m[\033[1;97m1\033[1;92m] \033[1;93mServo.net
+     \033[1;92m[\033[1;97m2\033[1;92m] \033[1;93mNgrok.io
+\033[1;92m[\033[1;97m#\033[1;92m] \033[1;93mRecommendation: \033[1;92mIf you will ask me for it, which one port forwarding is better then I'll recommend you 'Ngeok.io', Because ngrok is very easy to create a realistic phishing page.
+\033[1;92m[\033[1;97m#\033[1;92m] \033[1;93mMy Social Media ID
+\033[1;92m[\033[1;97m1\033[1;92m] Github:
+     (\033[1;97m★\033[1;92m)\033[1;93m Username:\033[1;92m qorsan taiz
+     (\033[1;97m√\033[1;92m)\033[1;93m Profile Link:\033[1;94m https://github.com/qorsan73
+\033[1;92m[\033[1;97m2\033[1;92m] Instagram:
+     (\033[1;97m★\033[1;92m)\033[1;93m Username:\033[1;92m qorsan taiz
+     (\033[1;97m√\033[1;92m)\033[1;93m Profile Link:\033[1;94m https://instagram.com/mostafa.qt73?igshid=YmMyMTA2M2Y=
+\033[1;92m[\033[1;97m3\033[1;92m] YouTube:
+     (\033[1;97m★\033[1;92m)\033[1;93m Channel Name:\033[1;92m qorsan taiz
+     (\033[1;97m√\033[1;92m)\033[1;93m Channel Link:\033[1;94m https://youtube.com/@hacking-world
+\033[1;92m[\033[1;97m4\033[1;92m] Telegram Channel:
+     (\033[1;97m★\033[1;92m)\033[1;93m Channel Name:\033[1;92m Qorsan taiz
+     (\033[1;97m√\033[1;92m)\033[1;93m Channel Link:\033[1;94m https://t.me/qorsantaez73/
+\033[1;92m[\033[1;97m5\033[1;92m] Telegram Chat:
+     (\033[1;97m★\033[1;92m)\033[1;93m Username:\033[1;92m qorsan taiz
+     (\033[1;97m√\033[1;92m)\033[1;93m Link For Chat:\033[1;94m https://t.me/qorsantaez73/
+"
+
+

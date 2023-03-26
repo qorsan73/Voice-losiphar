c4937e8b244e5b6b4c8433c9b78c50456f000cec
qorsan73 <99475446+qorsan73@users.noreply.github.com>
2023-03-26 04:27:58 +0300

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,40 @@
+clear
+echo -e "\033[1;96m               __                
+  ______ _____/  |_ __ ________  
+ /  ___// __ \   __\  |  \____ \ 
+ \___ \\  ___/|  | |  |  /  |_> >
+/____  >\___  >__| |____/|   __/ 
+     \/     \/           |__|    "
+sleep 1.0
+echo -e "\033[1;92m"
+apt update
+echo -e "\033[1;92m"
+apt upgrade
+echo -e "\033[1;92m"
+apt install wget -y
+echo -e "\033[1;92m"
+apt install curl -y
+echo -e "\033[1;92m"
+apt install python -y
+echo -r"\033[1;92m"
+apt install php -y
+echo -e "\033[1;92m"
+pip install php
+echo -e "\033[1;92m"
+apt install nodejs
+echo -e "\033[1;92m"
+npm install ngrok -g
+echo -e "\033[1;92m"
+apt install openssh -y
+echo -e "\033[1;92m"
+pip install ssh
+echo -e "\033[1;96m ____   __   __ _  ____ 
+(    \ /  \ (  ( \(  __)
+ ) D ((  O )/    / ) _) 
+(____/ \__/ \_)__)(____)"
+sleep 1.0
+echo -e '\033[1;92mTraper-X is launching......'
+sleep 1.1
+chmod +x *
+clear
+bash traperx.sh
